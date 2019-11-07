2e7393257312461e91d57cb811459a4b0f07c3b1
David <davidmunyiri2@gmail.com>
2019-11-07 16:48:07 +0300

    solved static files error

--- gis_dojo_blog/settings.py
+++ gis_dojo_blog/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'gis_dojo_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR, 'blog/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
