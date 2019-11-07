83a35b86803b34ab46eba6322766fde908d013cf
David <davidmunyiri2@gmail.com>
2019-11-07 13:26:04 +0300

    edited settings file

--- gis_dojo_blog/settings.py
+++ gis_dojo_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '+jcl=-34hd@_7c5il+d$2%gx&^)*(06+0pvi+@+jy+2n)61(sc'
+SECRET_KEY = '+jcl=-34hd@_7c5il+d$2%gx&^)*(06+0pvi+@+jy+2n)61(sc'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
